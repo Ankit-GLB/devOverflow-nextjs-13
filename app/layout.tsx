3acf31996d38446d33973860f276818eb070b62d
Ankit-GLB <ankitraj.ms11@gmail.com>
2023-12-09 23:12:26 +0530

    applied latest layout to the page

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,29 @@
 import { ClerkProvider } from "@clerk/nextjs";
 import React from "react";
+import type { Metadata } from "next";
+import { Inter, Space_Grotesk } from "next/font/google";
+import "../styles/theme.css";
+
+const inter = Inter({
+  subsets: ["latin"],
+  weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
+  variable: "--font-inter",
+});
+
+const spaceGrotesk = Space_Grotesk({
+  subsets: ["latin"],
+  weight: ["300", "400", "500", "600", "700"],
+  variable: "--fontGrotesk",
+});
+
+export const metadata: Metadata = {
+  title: "dev-overflow",
+  description:
+    "This is an open-source web-application where devloper and learners from all around the world can ask their queries and answer others queries and they together form a community which help and support each other.",
+  icons: {
+    icon: "/public/assets/images/site-logo.svg",
+  },
+};
 
 export default function RootLayout({
   children,
@@ -7,9 +31,19 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <ClerkProvider>
+    <ClerkProvider
+      appearance={{
+        elements: {
+          formButtonPrimary: "primary-gradient",
+          footerActionLink: "primary-text-gradient hover:text-primary-500",
+        },
+      }}
+    >
       <html lang="en">
-        <body>{children}</body>
+        <body className={`${inter.variable} ${spaceGrotesk.variable}`}>
+          <h1 className="h1-bold">this is a tempporary parat</h1>
+          {children}
+        </body>
       </html>
     </ClerkProvider>
   );
